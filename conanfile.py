d08976c348c03000039bfcd078b6d71d80fd2fbd
Shaul Fridman <shaul.fridman@gmail.com>
2019-12-14 06:29:23 +0200

    (+) 'conan build' works

--- conanfile.py
+++ conanfile.py
@@ -2,12 +2,12 @@ import os
 from os import path
 
 from conans import ConanFile, tools, AutoToolsBuildEnvironment
-from conans.tools import untargz, pythonpath, download, check_sha256
+from conans.tools import untargz, download, check_sha256
 
 
 class Recipe(ConanFile):
     name = "glibmm"
-    version = "2.58.1"  # "2.63.1"
+    version = "2.58.1"
     description = "glibmm, a C++ API for parts of glib that are useful for C++"
     license = "LGPL"
     url = "https://gitlab.com/no-face/glibmm-conan"  # recipe repo url
@@ -34,19 +34,20 @@ class Recipe(ConanFile):
     generators = "pkg_config"
 
     BASE_URL_DOWNLOAD = "https://download.gnome.org/sources/" + name
-    VER_MAGOR = "{}.{}".format(*version.split('.')[:2])
-    GLIBMM_FILE_BASE_NAME = "{}-{}".format(name, version)  # e.g. glibmm-2.63.1
-    FILE_URL = "{}/{}/{}.tar.xz".format(BASE_URL_DOWNLOAD, VER_MAGOR, GLIBMM_FILE_BASE_NAME)
+    VERSION_MAJOR = "{}.{}".format(*version.split('.')[:2])
+    FILE_BASE_NAME = "{}-{}".format(name, version)  # e.g. glibmm-2.58.1
+    FILE_URL = "{}/{}/{}.tar.xz".format(BASE_URL_DOWNLOAD, VERSION_MAJOR, FILE_BASE_NAME)
     FILE_SHA256 = '6e5fe03bdf1e220eeffd543e017fd2fb15bcec9235f0ffd50674aff9362a85f0'
 
     def source(self):
         zip_name = self.name + ".tar.xz"
         download(self.FILE_URL, zip_name)
         check_sha256(zip_name, self.FILE_SHA256)
-        untargz(zip_name)  # will extract sources to a folder named GLIBMM_FILE_BASE_NAME
+        untargz(zip_name)  # the zipped folder name is FILE_BASE_NAME. this will extract it.
         os.remove(zip_name)
 
     def build(self):
+        # glibmm autotools script expect sigc++-2.0.pc, so we rename the auto generated file: sigc++.pc
         sig_cpp_pc_old = os.path.join(self.build_folder, "sigc++.pc")
         sig_cpp_pc_new = os.path.join(self.build_folder, "sigc++-2.0.pc")
         if os.path.exists(sig_cpp_pc_old):
@@ -56,13 +57,13 @@ class Recipe(ConanFile):
         src = os.path.join(self.build_folder, self.GLIBMM_FILE_BASE_NAME)
         with tools.chdir(src), tools.environment_append({"PKG_CONFIG_PATH": self.build_folder}):
             autotools = AutoToolsBuildEnvironment(self)
-            autotools.libs.append('resolv')
+            autotools.libs.append('resolv')  # MacOS depend on libresolv for some reason
             is_shared = "yes" if self.options["shared"] else "no"
             is_static = "yes" if not self.options["shared"] else "no"
             args = [f"--enable-static={is_static}", f"--enable-shared={is_shared}"]
             autotools.configure(args=args)
             autotools.make()
-            autotools.install()
+            autotools.install()  # put all artifacts in folder name "package".
 
     def package(self):
         pass
